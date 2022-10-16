c77d1d1b97a36a59687403734767f490b627d55d
binayalenka <binaya.lenka348@gmail.com>
2022-10-16 19:54:42 +0530

    Update streamlit_app.py
    Added one more header menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 streamlit.title("My Parents New Healty Diner")
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Otameal')
-streamlit.text('kale , Spinach & Rocket Smoothy')
-streamlit.text('Hard Boiled Free-Range Egg')
+streamlit.text(' Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
